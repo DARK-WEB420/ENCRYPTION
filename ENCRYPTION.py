4e5816b6d353010393de5d40beab6a514a10fbf8
DARK-WEB420 <127849779+DARK-WEB420@users.noreply.github.com>
2024-03-28 21:58:08 +0600

    Update ENCRYPTION.py

--- ENCRYPTION.py
+++ ENCRYPTION.py
@@ -1,1 +1,21 @@
-
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from ENC import Main
+ 
+        Main()
